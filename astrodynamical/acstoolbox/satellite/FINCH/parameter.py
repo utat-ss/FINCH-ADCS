6919d327407648ca2e56b43ae56949bdac63d285
Khang Nguyen <62628865+khangaerospace@users.noreply.github.com>
2022-11-22 17:26:23 -0500

    FINCH parameter
    describe finch parameter

---
+++ astrodynamical/acstoolbox/satellite/FINCH/parameter.py
@@ -0,0 +1,13 @@
+"""FINCH Parameter"""
+
+__all__ = ["orbit_h", "mass", "sample_d", "SMA", "ECC", "INC", "RAAN"]
+
+orbit_h = 550000  # [m]
+mass = 4.2  # [kg]
+sample_d = 90  # [m]
+SMA = 6903  # average semi major axis [km]
+ECC = 0.003621614  # Eccentricity
+INC = 97.49588373  # average inclination [degree]
+RAAN = 227.05566156  # Right Ascension of Ascending Node [degree]
+
+
